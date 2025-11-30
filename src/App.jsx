34e8e0ea7049a4adb854a1ea256877ce6391df2c
Felipe Braga <felipebcarlos@yahoo.com.br>
2025-11-29 21:36:01 -0300

    Atualizacao do frontend para V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // URL do seu repositório
+  const repoUrl = "https://github.com/felipebcarlos/10CLDR_TF_DEVOPS_CI-CD";
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="container">
+      <header>
+        <h1>Trabalho Final DevOps</h1>
+        <h2>Turma 10CLDR</h2>
+      </header>
+
+      <main>
+        <p className="content-text">
+          Esta é uma página web estática. Desenvolvida para registrar a entrega do trabalho final da disciplina de DevOps CI/CD.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+        <p className="content-text">
+          Ela usa{' '}
+          <a href="https://nodejs.org/" target="_blank" rel="noreferrer" className="tech-link">NodeJs</a>,{' '}
+          <a href="https://vitejs.dev/" target="_blank" rel="noreferrer" className="tech-link">Vite</a>,{' '}
+          <a href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript" target="_blank" rel="noreferrer" className="tech-link">JavaScript</a>,{' '}
+          <a href="https://react.dev/" target="_blank" rel="noreferrer" className="tech-link">React</a> e{' '}
+          <a href="https://github.com/" target="_blank" rel="noreferrer" className="tech-link">Github</a>.
+        </p>
+
+        <div className="cta-section">
+          <p className="cta-text">
+            Se quiser fazer uma cópia para aprender, avaliar e customizar do seu jeito, clique no botão abaixo para ser redirecionado ao repositório.
+          </p>
+          
+          <a href={repoUrl} target="_blank" rel="noreferrer" className="github-button">
+            Ir para o Repositório no GitHub
+          </a>
+        </div>
+      </main>
+    </div>
   )
 }
 
